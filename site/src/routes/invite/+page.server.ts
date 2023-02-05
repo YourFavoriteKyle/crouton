6db020fc3fab0e9866682534e9fddff5f3caef84
Kyle Moore <20785882+YourFavoriteKyle@users.noreply.github.com>
2023-02-05 14:20:45 -0500

    Fix invite login call

--- site/src/routes/invite/+page.server.ts
+++ site/src/routes/invite/+page.server.ts
@@ -3,10 +3,10 @@ import { login } from '$lib/server/authHelpers';
 
 export const actions = {
 	default: async (event) => {
-		await login(event, 'invite', { permissions: '277025459200' });
+		await login(event, 'invite', undefined, { permissions: '277025459200' });
 	}
 } satisfies Actions;
 
 export const load = (async (event) => {
-	await login(event, 'invite', { permissions: '277025459200' });
+	await login(event, 'invite', undefined, { permissions: '277025459200' });
 }) satisfies PageServerLoad;
