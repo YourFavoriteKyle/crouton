0934b7827c247b5ed76ceced24a79a32edefe704
Kyle Moore <20785882+YourFavoriteKyle@users.noreply.github.com>
2023-02-24 11:28:06 -0500

    Update vercel analytics env var

--- site/src/routes/+layout.server.ts
+++ site/src/routes/+layout.server.ts
@@ -1,10 +1,10 @@
 import type { LayoutServerLoad } from './$types';
-import { VITE_VERCEL_ANALYTICS_ID } from '$env/static/private';
+import { VERCEL_ANALYTICS_ID } from '$env/static/private';
 import { getServerSession } from '@supabase/auth-helpers-sveltekit';
 
 export const load = (async (event) => {
 	return {
-		vercelVitalsID: VITE_VERCEL_ANALYTICS_ID,
+		vercelVitalsID: VERCEL_ANALYTICS_ID,
 		session: await getServerSession(event)
 	};
 }) satisfies LayoutServerLoad;
