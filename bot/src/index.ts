02b2e47b337d6873f8ded82465e456f463528bee
Kyle Moore <20785882+YourFavoriteKyle@users.noreply.github.com>
2023-05-10 18:51:00 -0400

    Update formatting

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,28 +1,28 @@
-import { readdirSync } from "fs";
-import { join } from "path";
-import { config } from "dotenv";
-import { SlashCommand } from "./types";
-import { Client, Collection, GatewayIntentBits, Partials } from "discord.js";
+import { readdirSync } from 'fs';
+import { join } from 'path';
+import { config } from 'dotenv';
+import { SlashCommand } from './types';
+import { Client, Collection, GatewayIntentBits, Partials } from 'discord.js';
 
-const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : ".env";
-config({ path: join(__dirname, "..", "..", envFile) });
+const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
+config({ path: join(__dirname, '..', '..', envFile) });
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.GuildMessages,
-  ],
-  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMessages
+	],
+	partials: [Partials.Message, Partials.Channel, Partials.Reaction]
 });
 
 client.slashCommands = new Collection<string, SlashCommand>();
 client.cooldowns = new Collection<string, number>();
 
-const handlersDir = join(__dirname, "./handlers");
+const handlersDir = join(__dirname, './handlers');
 readdirSync(handlersDir).forEach((handler) => {
-  require(`${handlersDir}/${handler}`)(client);
+	require(`${handlersDir}/${handler}`)(client);
 });
 
 client.login(process.env.BOT_TOKEN);
